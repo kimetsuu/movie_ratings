e524aa2aaed564568380fe602fadaba0afe846c9
Denis Novikov <dnovikov2105@gmail.com>
2024-12-27 21:01:53 +0100

    as

--- ratings.py
+++ ratings.py
@@ -4,22 +4,22 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv('/Users/denisnovikov/Downloads/title.akas.tsv', sep = '\t')
 
-# clean the data
-df = df.dropna()
-df['column_name'] = df['column_name'].fillna('default_value')
+# # clean the data
+# df = df.dropna()
+# df['column_name'] = df['column_name'].fillna('default_value')
 
-df = df.drop_duplicates()
+# df = df.drop_duplicates()
 
 
 # print(df['genre'].value_counts())
 
-# graph
-
-# df['rating'].hist(bins=20)
-# plt.xlabel('Rating')
-# plt.ylabel('Frequency')
-# plt.title('Distribution of Ratings')
-# plt.show()
-
 print(df.columns)
 print(df.head())
+
+# graph
+
+
+
+
+
+

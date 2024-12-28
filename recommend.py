8e2734835902ec62b7906f30befe4e8443d11be9
Denis Novikov <dnovikov2105@gmail.com>
2024-12-28 15:18:07 +0100

    added sorting by type and genre

--- recommend.py
+++ recommend.py
@@ -3,15 +3,26 @@ import pandas as pd
 
 df = pd.read_csv('/Users/denisnovikov/Downloads/title.akas.tsv', sep = '\t')
 df_ratings = pd.read_csv('/Users/denisnovikov/Downloads/title.ratings.tsv', sep = '\t')
+df_genres = pd.read_csv('/Users/denisnovikov/Downloads/title.basics.tsv', sep = '\t', low_memory = False)
 
 merged = pd.merge(df, df_ratings, left_on = 'titleId', right_on = 'tconst')
+merged = pd.merge(merged, df_genres, left_on = 'titleId', right_on = 'tconst')
 
-merged = merged[['titleId', 'title', 'averageRating', 'numVotes']]
+merged = merged[['titleId', 'title', 'averageRating', 'numVotes', 'titleType', 'genres']]
 
 min_rating = float(input('Enter the minimum rating: '))
 n_movies = int(input('Enter the number of movies to recommend: '))
+movie_type = input('Enter the movie type (movie, short, tvseries, tvepisode, video): ')
+genre = input('Enter the genre: ')
 
-filtered_movies = merged[merged['averageRating'] >= min_rating]
+filtered_movies = merged[
+    (merged['averageRating'] >= min_rating) & 
+    (merged['titleType'].str.lower() == movie_type.lower()) & 
+    (merged['genres'].str.lower().fillna('').apply(lambda g: genre.lower() in g.split(',')))
+]
+
+print(len(filtered_movies))
+
 
 selected = []
 total = len(filtered_movies)
@@ -34,4 +45,4 @@ else:
     selected_df = filtered_movies
     
 print('\nSelected Movies:')
-print(selected_df[['title', 'averageRating']])
+print(selected_df[['title', 'averageRating', 'titleType', 'genres']])
